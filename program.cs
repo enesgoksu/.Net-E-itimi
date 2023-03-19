2a5bce5449776ff15a7f3c9f71dc8347950f440c
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-03-19 17:50:47 +0300

    program.cs değiştirildi

---
+++ program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace değişkenler
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int deger = 2;
+            string degisken = null;
+            string Degisken = null;
+            Console.WriteLine(deger);
+            Console.WriteLine(degisken);
+        }
+    }
+}
