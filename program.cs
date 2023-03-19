fcd7f9a084efd01cf02a2041e1d6c733bdf683c9
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-03-19 17:57:47 +0300

    program.cs değiştirildi

--- program.cs
+++ program.cs
@@ -6,11 +6,16 @@ namespace değişkenler
     {
         public static void Main(string[] args)
         {
-            int deger = 2;
-            string degisken = null;
-            string Degisken = null;
-            Console.WriteLine(deger);
-            Console.WriteLine(degisken);
+            console.WriteLine("");
+            byte b = 5;     // 1 byte
+            sbyte c = 5;    // 1 byte
+            short s = 5;    // 2 byte
+            ushort us =5;   // 2 byte
+            Int16 i16 = 2;  // 2 byte
+            int i = 2;      // 4 byte
+            Int32 i32 = 2;  // 4 byte
+            Int64 i64 = 2 ; // 8 byte
+            
         }
     }
 }
